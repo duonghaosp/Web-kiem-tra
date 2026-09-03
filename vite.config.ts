a8df9e6cabf3bfa09d190c2a1572f62eb3681203
duonghaosp <duonghaosp@users.noreply.github.com>
2026-09-03 07:36:20 +0700

    Cấu hình máy chủ Vite mở cổng mạng nội bộ (host) giúp điện thoại kết nối thử nghiệm dễ dàng

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
